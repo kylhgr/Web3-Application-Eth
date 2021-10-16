36a75707b267a1e733089cb3a8e4887b02779077
Kaylah Gore <43583709+kylhgr@users.noreply.github.com>
2021-10-16 16:55:51 +0000

    Confirmed the contract has deployed, ran and updated the script file.

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED 
 
-pragma solidity ^0.88.0;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
   constructor() {
-    console.log("Kaylah has entered the Metaverse")
+    console.log("Kaylah has entered the Metaverse");
   }
 }
