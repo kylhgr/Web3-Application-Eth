14af844f3f1396c357012569bf19108c99c74e10
Kaylah Gore <43583709+kylhgr@users.noreply.github.com>
2021-10-16 16:40:31 +0000

    Adding test smart contract and deployment script

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED 
+
+pragma solidity ^0.88.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+  constructor() {
+    console.log("Kaylah has entered the Metaverse")
+  }
+}
